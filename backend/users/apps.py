0270b1e211cd0abf7cfc6d845d674585f7de68db
Mehrshad Valizadeh Arjmand <meh83valiz@proton.me>
2026-02-27 20:55:07 +0330

    Revert "update users/apps.py"
    This reverts commit 02988fb4221e7c5e4569ab2d2921c2928c540915.

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,23 +1,6 @@
-#backend/users/apps.py
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
+
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    def ready(self):
-        from django.contrib.auth.models import Group
-        groups = [
-            'Detective',
-            'Sergeant',
-            'Captain',
-            'Chief',
-            'Coroner',
-            'Judge',
-            'Police Officer',
-            'Patrol Officer',
-            'Cadet',
-        ]
-        for group_name in groups:
-            Group.objects.get_or_create(name=group_name)
